4cc5643679dbea94290cdb68258d97736a974113
sapk2 <sapkotap521@gmail.com>
2025-02-28 15:02:32 +0545

    comit

--- day1/index.js
+++ day1/index.js
@@ -2,5 +2,9 @@ document.addEventListener('alpine:init',()=>{
     Alpine.data('counter',()=>({
         count:0,
         name:'mario',
+
+        logcount(){
+            console.log(this.count)
+        },
     }))
 })
